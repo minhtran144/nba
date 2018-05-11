cf8c60aec505dee39baf47a5db59d74742f7f191
minhtran144 <37429377+minhtran144@users.noreply.github.com>
2018-05-11 13:18:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var express  = require('express'),
 
 app = express(),
     
-app.get(function(req,res) {
+function respond(req, res) {
     res.send('We are happy to see you using Chat Bot Webhook');
-  });
+};
+    
+app.get('/', respond);
 
 app.listen((process.env.PORT || 8000), function () {
    console.log("Server is up and listening on port");
