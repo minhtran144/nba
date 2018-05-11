0061ad037e2cb070f33225fd83f097570bdb1bec
minhtran144 <37429377+minhtran144@users.noreply.github.com>
2018-05-11 13:06:50 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+var app = express(),
+    
+app.get('/',function(req,res){
+    res.send('We are happy to see you using Chat Bot Webhook');
+  });
+
+app.listen((process.env.PORT || 8000), function () {
+   console.log("Server is up and listening on port");
+ 
